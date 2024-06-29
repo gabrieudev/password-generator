5cf8f35188fb2901f618d38cda45af31bf57bd1b
João Gabriel da Conceição Santos <gs495770@gmail.com>
2024-06-29 14:50:05 -0300

    initial commit

---
+++ src/main/java/com/api/password_generator/PasswordGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.api.password_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordGeneratorApplication.class, args);
+	}
+
+}
